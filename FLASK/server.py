209ba80973a27e1b9e79df6197338c49c279c8c0
Rajapandi K <krajapandi.developer@gmail.com>
2025-02-28 20:47:01 +0530

    Update server.py

--- FLASK/server.py
+++ FLASK/server.py
@@ -7,29 +7,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/index.html')
-# def index2():
-#     return render_template('index.html')
+@app.route('/index.html')
+def index2():
+   return render_template('index.html')
 
-# @app.route('/about.html')
-# def about():
-#     return render_template('about.html')
+@app.route('/about.html')
+ def about():
+    return render_template('about.html')
 
-# @app.route('/works.html')
-# def works():
-#     return render_template('works.html')
+@app.route('/works.html')
+def works():
+     return render_template('works.html')
 
-# @app.route('/work.html')
-# def work():
-#     return render_template('work.html')
+ @app.route('/work.html')
+ def work():
+     return render_template('work.html')
 
-# @app.route('/components.html')
-# def components():
-#     return render_template('components.html')
-
-# @app.route('/contact.html')
-# def contact():
-#     return render_template('contact.html')
+ @app.route('/components.html')
+ def components():
+          return render_template('components.html')
+     
+ @app.route('/contact.html')
+ def contact():
+     return render_template('contact.html')
 
 @app.route('/<string:page_name>')
 def html_pages(page_name):
